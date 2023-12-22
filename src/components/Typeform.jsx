3fba909e4b5950f75b4d1f51eb403b619f22c14b
Doğukan Topçu <dogukantopcu35@gmail.com>
2023-12-22 05:54:14 +0300

    router.push url changed for correction

--- src/components/Typeform.jsx
+++ src/components/Typeform.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Typeform = () => {
     const router = useRouter();
   return (
-    <div onClick={() => router.push("/supportus")} className='cool-border rounded-md min-w-[207px] p-3 text-center cursor-pointer text-[10px]'>
+    <div onClick={() => router.push("/form")} className='cool-border rounded-md min-w-[207px] p-3 text-center cursor-pointer text-[10px]'>
         Support us develop our product
     </div>
   )
