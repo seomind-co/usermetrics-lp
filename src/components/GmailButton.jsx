8dcc6284e6e0fcbce3f55c51ce0ddbe86a9f7d13
Doğukan Topçu <dogukantopcu35@gmail.com>
2023-12-22 05:58:00 +0300

    google button takes to 10

--- src/components/GmailButton.jsx
+++ src/components/GmailButton.jsx
@@ -5,7 +5,7 @@ import Google from "../assets/img/googleLogo.svg";
 
 const GmailButton = () => {
   return (
-    <div className='px-4'>
+    <div className='px-4 -mt-10'>
         <a href="https://mail.google.com/mail/" target='_blank' rel="noopener noreferrer" className='googleButton flex justify-center items-center gap-3 sm:rounded-[20px] rounded-[12px] sm:px-16 px-8 py-4 sm:text-[16px] text-[14px]'>
             <Image src={Google} alt='Gmail' />
             Open Gmail
